ac48565bd623df26f348fcbe31d4b39232702609
root <root@photon01.lab.fcc.gov>
2015-05-12 17:22:01 +0000

    Prepare environment and generate a docker-compose.yml file.

--- setEnv.sh
+++ setEnv.sh
@@ -3,9 +3,17 @@
 # Initial written date: 2015-05-12
 #
 # Using etcd set variables used by confd to gernerate the docker-compose.yml file for the ci project
+# This script contains 3 steps: 
+# (1) prepare_environment: to install pre-requisites software and config files;
+# (2) set_varibles:  to set variables needed for this project;
+# (3) generate_config_file:  to generate a /etc/confd/conf.d/docker-compose.yml file for this project;
 #
 
+function prepare_environment() {
 # This script need etcdctl, confd, and docker-compose installed.
+echo "(1) Check if etcdctl, confd and docker-compoase is installed and a new coreos/etcd container is running..."
+echo
+
 if [ ! -d /usr/local ]; then
     mkdir -p /usr/local
 fi
@@ -14,7 +22,7 @@ fi
 if [ -x "/usr/bin/etcdctl" ]; then
     echo "etcdctl is available at: /usr/bin/etcdctl"
 else
-    curl -L  https://github.com/coreos/etcd/releases/download/v2.1.0-alpha.0/etcd-v2.1.0-alpha.0-linux-amd64.tar.gz -o /usr/local/etcd-v2.1.0-alpha.0-linux-amd64.tar.gz
+    curl -sL  https://github.com/coreos/etcd/releases/download/v2.1.0-alpha.0/etcd-v2.1.0-alpha.0-linux-amd64.tar.gz -o /usr/local/etcd-v2.1.0-alpha.0-linux-amd64.tar.gz
     cd /usr/local && tar xzf /usr/local/etcd-v2.1.0-alpha.0-linux-amd64.tar.gz
     chmod +x /usr/local/etcd-v2.1.0-alpha.0-linux-amd64/etcdctl
     ln -s /usr/local/etcd-v2.1.0-alpha.0-linux-amd64/etcdctl /usr/bin/etcdctl
@@ -24,7 +32,7 @@ fi
 if [ -x "/usr/bin/confd" ]; then
     echo "confd is available at: /usr/bin/confd"
 else
-    curl -L https://github.com/kelseyhightower/confd/releases/download/v0.9.0/confd-0.9.0-linux-amd64 -o /usr/local/confd-0.9.0-linux-amd64
+    curl -sL https://github.com/kelseyhightower/confd/releases/download/v0.9.0/confd-0.9.0-linux-amd64 -o /usr/local/confd-0.9.0-linux-amd64
     chmod +x /usr/local/confd-0.9.0-linux-amd64
     ln -s /usr/local/confd-0.9.0-linux-amd64 /usr/bin/confd
 fi
@@ -33,26 +41,41 @@ fi
 if [ -x "/usr/bin/docker-compose" ]; then
     echo "docker-compose is available at: /usr/bin/docker-compose"
 else
-    curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/docker-compose
+    curl -sL https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/docker-compose
     chmod +x /usr/local/docker-compose
     ln -s /usr/local/docker-compose /usr/bin/docker-compose
 fi
 
+# Retrieve the docker-compose template files 
+echo "Retrieving docker-compose template files"
+mkdir -p /etc/confd/{templates,conf.d}
+curl -sL https://github.com/kfmaster/ci/blob/docker-compose/docker-compose.yml.example -o /etc/confd/conf.d/docker-compose.yml.example
+curl -sL https://github.com/kfmaster/ci/blob/docker-compose/docker-compose.tmpl -o /etc/confd/templates/docker-compose.tmpl
+curl -sL https://github.com/kfmaster/ci/blob/docker-compose/docker-compose.toml -o /etc/confd/conf.d/docker-compose.toml
+
+
 # Start a sidekick container based on coreos/etcd to store user variables
+echo
 docker ps |grep -q coreos/etcd
 if [ $? -eq 0 ]; then
     echo "There are coreos/etcd containers running, stop them to begin fresh"
     docker ps |grep coreos/etcd |awk '{print $1}' |xargs docker stop
 fi
 
+echo "Straring a coreos/etcd container to store variables"
 docker run \
 -p 4001:4001 \
 -d coreos/etcd
 
 echo "Waiting 10 seconds for the etcd container to come up..."
 sleep 10
+echo
+}
 
+function set_varibles() {
 # First create naming spaces for etcd variables
+echo "(2) Set variables using etcdctl ..."
+echo
 for svc in /services /services/datagerrit /services/datajenkins /services/pggerrit /services/gerrit /services/jenkins /services/pgredmine /services/redmine /services/nginxproxy
 do
     etcdctl ls $svc 1> /dev/null 2>&1
@@ -114,7 +137,20 @@ etcdctl set /services/redmine/image sameersbn/redmine
 etcdctl set /services/nginxproxy/image nginx
 etcdctl set /services/nginxproxy/name nginx-proxy
 etcdctl set /services/nginxproxy/volumes ~/nginx-docker/proxy.conf:/etc/nginx/conf.d/proxy.conf:ro
+echo
+}
 
+function generate_config_file() {
 # confd program will use config files in /etc/confd/{templates,conf.d} to generate the docker-compose.yml file needed by this project
 # Please check the files in /etc/confd/templates and /etc/confd/conf.d for more details
+echo "(3) Using confd to gerenate a docker-compose.yml file at the default destination"
+echo
+
 confd -onetime -backend etcd -node 127.0.0.1:4001
+echo
+echo "Please check if /etc/confd/conf.d/docker-compose.yml generated as expected and run docker-compoase against it."
+}
+
+prepare_environment
+set_varibles
+generate_config_file
