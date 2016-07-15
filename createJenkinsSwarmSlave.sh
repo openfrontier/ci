333e893bbc7193bb0d9a739faaca4805cfbfc1ed
thinkernel <thinkernel@gmail.com>
2016-07-15 12:17:00 +0000

    Make Jenkins slave node support docker network

--- createJenkinsSwarmSlave.sh
+++ createJenkinsSwarmSlave.sh
@@ -4,9 +4,11 @@ set -e
 NEXUS_REPO=${NEXUS_REPO:-$1}
 JENKINS_SLAVE_IMAGE=${JENKINS_SLAVE_IMAGE:-openfrontier/jenkins-swarm-maven-slave}
 LABEL=${LABEL:-swarm}
+CI_NETWORK=${CI_NETWORK:-ci-network}
 
 docker run \
   -e NEXUS_REPO=${NEXUS_REPO} \
+  --net=${CI_NETWORK} \
   -d ${JENKINS_SLAVE_IMAGE} \
   -labels ${LABEL} \
   -mode exclusive \
