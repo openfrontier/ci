8e98f75ac4d06598eedd90485e0cde3ebad8c265
thinkernel <thinkernel@gmail.com>
2016-07-18 06:16:39 +0000

    Check existence before creating the network

--- createContainer.sh
+++ createContainer.sh
@@ -6,7 +6,9 @@ source ~/ci/config
 source ~/ci/config.default
 
 # Create ci network
+if [ -z "$(docker network ls | grep ${CI_NETWORK})" ]; then
   docker network create ${CI_NETWORK}
+fi
 
 # Create Nexus server.
 if [ ${#NEXUS_WEBURL} -eq 0 ]; then
