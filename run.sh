787d1ae078eb11305486575d84aa5096d9ba42ec
thinkernel <thinkernel@gmail.com>
2017-02-24 06:57:30 +0000

    Switch images according to ci-compose project

--- run.sh
+++ run.sh
@@ -12,31 +12,34 @@ if [ ! -e "${SSH_KEY_PATH}" -o ! -e "${SSH_KEY_PATH}.pub" ]; then
 fi
 
 ~/ci/createContainer.sh ${SUFFIX}
-while [ -z "$(docker logs ${GERRIT_NAME} 2>&1 | grep "Creating user:")" ]; do
+while [ -z "$(docker logs ${NGINX_NAME} 2>&1 | grep "gerrit ready")" ]; do
     echo "Waiting gerrit ready."
     sleep 1
 done
 echo "Gerrit is ready"
-while [ -z "$(docker logs ${JENKINS_NAME} 2>&1 | grep "setting agent port for jnlp")" ]; do
+
+while [ -z "$(docker logs ${NGINX_NAME} 2>&1 | grep "jenkins ready")" ]; do
     echo "Waiting jenkins ready."
     sleep 1
 done
 echo "Jenkins is ready"
+
 while [ -z "$(docker logs ${REDMINE_NAME} 2>&1 | grep "INFO success: nginx entered RUNNING state")" ]; do
     echo "Waiting redmine ready."
     sleep 1
 done
 echo "Redmine is ready"
+
+while [ -z "$(docker logs ${NGINX_NAME} 2>&1 | grep "nexus ready")" ]; do
+    echo "Waiting nexus ready."
+    sleep 1
+done
+echo "Nexus is ready"
+
 #sleep 5
 ~/ci/setupContainer.sh ${SUFFIX}
 #sleep 10
-while [ -n "$(docker logs ${JENKINS_NAME} 2>&1 | tail -n 5 | grep 'Running from: /usr/share/jenkins/jenkins.war')" -o \
-        -z "$(docker logs ${JENKINS_NAME} 2>&1 | tail -n 5 | grep 'Will now listen for new projects...')" ]; do
-    echo "Waiting jenkins ready."
-    sleep 1
-done
-echo "Jenkins is ready"
-#sleep 5
+echo ">>>> Start import demo projects"
 ~/ci/importDemoProject.sh ${SUFFIX}
 ~/ci/importDockerProject.sh ${SUFFIX}
 
